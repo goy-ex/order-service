589672a0c7ad95a1e00d39a268d331ade003ca82
akhmy <akhmy.work@mail.ru>
2026-08-08 17:10:49 +0300

    feat(domain): add tests

--- internal/domain/domain_test.go
+++ internal/domain/domain_test.go
@@ -4,74 +4,58 @@ import (
 	"testing"
 
 	"github.com/goy-ex/order-service/internal/domain"
-	"github.com/goy-ex/order-service/internal/pkg/fixture"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
-//nolint:funlen // tabletest
-func TestOrder_New(t *testing.T) {
+func TestNewAsset(t *testing.T) {
 	t.Parallel()
 
 	type testcase struct {
-		checkErr func(t *testing.T, err error)
-		order    domain.Order
+		symbol   string
+		name     string
+		decimals int
+		check    func(t *testing.T, asset *domain.Asset, err error)
 	}
 
 	testcases := map[string]testcase{
 		"valid": {
-			order: fixture.NewValidOrder(),
-			checkErr: func(t *testing.T, err error) {
+			symbol:   "BTC",
+			name:     "Bitcoin",
+			decimals: 8,
+			check: func(t *testing.T, asset *domain.Asset, err error) {
 				t.Helper()
 
 				require.NoError(t, err)
+				assert.Equal(t, "BTC", asset.Symbol)
+				assert.Equal(t, "Bitcoin", asset.Name)
+				assert.Equal(t, 8, asset.Decimals)
 			},
 		},
-		"bad_price": {
-			order: func() domain.Order {
-				valid := fixture.NewValidOrder()
-				valid.Price = 0
-
-				return valid
-			}(),
-			checkErr: func(t *testing.T, err error) {
+		"decimals_zero": {
+			symbol:   "BTC",
+			name:     "Bitcoin",
+			decimals: 0,
+			check: func(t *testing.T, asset *domain.Asset, err error) {
 				t.Helper()
 
-				var target *domain.FieldNotPositiveError
+				var target *domain.NotPositiveNumberError
 				require.ErrorAs(t, err, &target)
-				require.Equal(t, "Price", target.FieldName)
-				require.Equal(t, 0, target.Value)
+				assert.Equal(t, "Decimals", target.FieldName)
+				assert.Equal(t, 0, target.Value)
 			},
 		},
-		"bad_amount": {
-			order: func() domain.Order {
-				valid := fixture.NewValidOrder()
-				valid.Amount = 0
-
-				return valid
-			}(),
-			checkErr: func(t *testing.T, err error) {
+		"decimals_negative": {
+			symbol:   "BTC",
+			name:     "Bitcoin",
+			decimals: -1,
+			check: func(t *testing.T, asset *domain.Asset, err error) {
 				t.Helper()
 
-				var target *domain.FieldNotPositiveError
+				var target *domain.NotPositiveNumberError
 				require.ErrorAs(t, err, &target)
-				require.Equal(t, "Amount", target.FieldName)
-				require.Equal(t, 0, target.Value)
-			},
-		},
-		"bad_remaining": {
-			order: func() domain.Order {
-				valid := fixture.NewValidOrder()
-				valid.Remaining = 0
-
-				return valid
-			}(),
-			checkErr: func(t *testing.T, err error) {
-				t.Helper()
-
-				var target *domain.FieldNotPositiveError
-				require.ErrorAs(t, err, &target)
-				require.Equal(t, "Remaining", target.FieldName)
-				require.Equal(t, 0, target.Value)
+				assert.Equal(t, "Decimals", target.FieldName)
+				assert.Equal(t, -1, target.Value)
 			},
 		},
 	}
@@ -80,88 +64,28 @@ func TestOrder_New(t *testing.T) {
 		t.Run(name, func(t *testing.T) {
 			t.Parallel()
 
-			_, err := domain.NewOrder(
-				tc.order.ID,
-				tc.order.UserID,
-				tc.order.Pair,
-				tc.order.Side,
-				tc.order.Price,
-				tc.order.Amount,
-				tc.order.Remaining,
-				tc.order.CreatedAt,
-			)
-			tc.checkErr(t, err)
+			asset, err := domain.NewAsset(tc.symbol, tc.name, tc.decimals)
+			tc.check(t, asset, err)
 		})
 	}
 }
 
-//nolint:funlen // tabletest
-func TestPair_New(t *testing.T) {
+func TestEventTypeFromString(t *testing.T) {
 	t.Parallel()
 
 	type testcase struct {
-		checkErr func(t *testing.T, err error)
-		pair     domain.Pair
+		str  string
+		want bool
 	}
 
 	testcases := map[string]testcase{
 		"valid": {
-			pair: fixture.NewValidPair(),
-			checkErr: func(t *testing.T, err error) {
-				t.Helper()
-				require.NoError(t, err)
-			},
+			str:  domain.EventTypeOrderCreated.String(),
+			want: true,
 		},
-		"invalid_status": {
-			pair: func() domain.Pair {
-				valid := fixture.NewValidPair()
-				valid.Status = 42
-
-				return valid
-			}(),
-			checkErr: func(t *testing.T, err error) {
-				t.Helper()
-
-				var target *domain.NoSuchOptionError
-
-				require.ErrorAs(t, err, &target)
-				require.Equal(t, "Status", target.FieldName)
-				require.Equal(t, "invalid(42)", target.Value)
-			},
-		},
-		"bad_price_tick": {
-			pair: func() domain.Pair {
-				valid := fixture.NewValidPair()
-				valid.PriceTick = 0
-
-				return valid
-			}(),
-			checkErr: func(t *testing.T, err error) {
-				t.Helper()
-
-				var target *domain.FieldNotPositiveError
-
-				require.ErrorAs(t, err, &target)
-				require.Equal(t, "PriceTick", target.FieldName)
-				require.Equal(t, 0, target.Value)
-			},
-		},
-		"bad_qty_tick": {
-			pair: func() domain.Pair {
-				valid := fixture.NewValidPair()
-				valid.QtyTick = 0
-
-				return valid
-			}(),
-			checkErr: func(t *testing.T, err error) {
-				t.Helper()
-
-				var target *domain.FieldNotPositiveError
-
-				require.ErrorAs(t, err, &target)
-				require.Equal(t, "QtyTick", target.FieldName)
-				require.Equal(t, 0, target.Value)
-			},
+		"invalid": {
+			str:  "invalid",
+			want: false,
 		},
 	}
 
@@ -169,14 +93,97 @@ func TestPair_New(t *testing.T) {
 		t.Run(name, func(t *testing.T) {
 			t.Parallel()
 
-			_, err := domain.NewPair(
-				tc.pair.Base,
-				tc.pair.Quote,
-				tc.pair.PriceTick,
-				tc.pair.QtyTick,
-				tc.pair.Status,
-			)
-			tc.checkErr(t, err)
+			et := domain.EventTypeFromString(tc.str)
+			require.Equal(t, et.IsValid(), tc.want)
+		})
+	}
+}
+
+func TestEventType_String(t *testing.T) {
+	t.Parallel()
+
+	type testcase struct {
+		et   domain.EventType
+		want string
+	}
+
+	testcases := map[string]testcase{
+		"valid": {
+			et:   domain.EventTypeOrderCreated,
+			want: domain.EventTypeOrderCreated.String(),
+		},
+		"invalid_from_string": {
+			et:   domain.EventTypeFromString("invalid"),
+			want: "invalid",
+		},
+		"invalid_from_number": {
+			et:   domain.EventType(100),
+			want: "invalid(100)",
+		},
+	}
+
+	for name, tc := range testcases {
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			require.Equal(t, tc.want, tc.et.String())
+		})
+	}
+}
+
+func TestSideFromString(t *testing.T) {
+	t.Parallel()
+
+	type testcase struct {
+		str  string
+		want string
+	}
+
+	tcs := map[string]testcase{
+		"bid": {
+			str:  domain.SideBid.String(),
+			want: domain.SideBid.String(),
+		},
+		"ask": {
+			str:  domain.SideAsk.String(),
+			want: domain.SideAsk.String(),
+		},
+		"invalid": {
+			str:  "invalid",
+			want: "invalid",
+		},
+	}
+
+	for name, tc := range tcs {
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			s := domain.SideFromString(tc.str)
+			require.Equal(t, tc.want, s.String())
+		})
+	}
+}
+
+func TestSide_String(t *testing.T) {
+	t.Parallel()
+
+	type testcase struct {
+		side domain.Side
+		want string
+	}
+
+	tcs := map[string]testcase{
+		"invalid_from_number": {
+			side: domain.Side(100),
+			want: "invalid(100)",
+		},
+	}
+
+	for name, tc := range tcs {
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			require.Equal(t, tc.want, tc.side.String())
 		})
 	}
 }
